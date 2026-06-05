854b4d4c201fb9f2da6f5ce7be4a452337374a21
Jai <jaikishannayak@gmail.com>
2026-06-05 16:46:50 +0530

    feat: integrate Google OAuth for authentication and enhance user model

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 import { config } from "./config.js";
 
 const connectDB = async () => {
-    await mongoose.connect(config.MONGO_URI);
+    await mongoose.connect(config.MONGO_URI, {
+        dbName: "snitch",
+    });
+
     console.log("MongoDB connected");
 };
 
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -5,6 +5,7 @@ import {
 } from "../validator/auth.validator.js";
 import { googleCallback, login, register } from "../controllers/auth.controller.js";
 import passport from "passport";
+import { config } from "../config/config.js";
 
 const router = Router();
 
@@ -37,10 +38,12 @@ router.get(
  * @desc Google OAuth callback
  * @access Public
  */
-router.get(
-  "/google/callback",
-  passport.authenticate("google", { session: false }),
-  googleCallback,
-);
+router.get("/google/callback",
+    passport.authenticate("google", {
+        session: false,
+        failureRedirect: config.NODE_ENV == "development" ? "http://localhost:5173/login" : "/login"
+    }),
+    googleCallback,
+)
 
 export default router;
